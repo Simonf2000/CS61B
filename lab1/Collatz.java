df91fd9d700980d481304d37218a4b5a53b3e40f
simonf2000 <simonf2000@outlook.com>
2024-02-02 11:45:42 +0800

    lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,14 +3,19 @@
  */
 public class Collatz {
 
+    /**
+    * @Description:
+    * @Param: 
+    * @return: 
+    * @Author: simonf
+    * @Date: 2024/2/2
+    */
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (n  % 2 ==0) {
+            return n/2;
         } else {
-            return n * 2;
+            return 3 * n + 1;
         }
     }
 
